6b057b5795c819610bb7e2bb12a7e2e5bc8c6b87
Nguyen-Bach <nguyenhkin@gmail.com>
2023-05-08 13:59:45 +0700

    sdf

--- wk8/q4/src/main/java/com/example/q4/HelloController.java
+++ wk8/q4/src/main/java/com/example/q4/HelloController.java
@@ -2,13 +2,24 @@ package com.example.q4;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Circle question5Circle;
+    @FXML
+    private  Label question5Label;
+    @FXML
+    private Label question5Labelmove;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onQuestion5MouseEntered() {
+        question5Label.setText("inside");
     }
+    @FXML
+    protected void onQuestion5MouseExit() {
+        question5Label.setText("outside");
+    }
+
+
 }
